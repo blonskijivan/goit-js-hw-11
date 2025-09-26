3e85a768e4dbddc784ba26d4f36944a61031dbd1
Ivan <blonskijivan@gmail.com>
2025-09-26 20:16:01 +0300

    fix: define global+process.env for GH Pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: '/goit-js-hw-11/',   // для GitHub Pages
-  root: 'src',               // index.html всередині src
-  build: { outDir: '../dist', emptyOutDir: true }
+  base: '/goit-js-hw-11/',
+  root: 'src',
+  build: { outDir: '../dist', emptyOutDir: true },
+  define: {
+    global: 'window',
+    'process.env': {},   // на випадок, якщо щось читає process.env
+  },
 });
